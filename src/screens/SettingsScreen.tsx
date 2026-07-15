69c3d9b77eb9cff7a9cc209ce47e30da0382fb98
Peter Butkovic <butkovic@gmail.com>
2026-07-15 11:01:58 +0200

    chore: release v1.1.1

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -20,7 +20,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const LANGUAGE_STORAGE_KEY = storageKey('language');
 
 
-const APP_VERSION = '1.1.0';
+const APP_VERSION = '1.1.1';
 
 // Release notes for the current version on GitHub. Tags follow the `vX.Y.Z`
 // convention created by the release workflow.
